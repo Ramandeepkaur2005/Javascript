b1dc56287686d1a9b03d8a80c8f7359cb98d236e
Ramandeepkaur2005 <ramandeepkaur27092005@gmail.com>
2024-01-27 13:45:15 +0530

    New changes

---
+++ AIC-JavaScript/WEEK-1/DAY-1/varriable.js
@@ -0,0 +1,19 @@
+//declaring variables using 'let'
+let fullname="Ramandeep Kaur";
+console.log(fullname);
+let age=18;
+console.log(age);
+let year="2005";
+console.log(year);
+let x=25
+console.log(x);
+//declaring variables using 'var'
+var month=4;
+console.log(month);
+var month=5;
+console.log(month);
+//declaring variables using 'const':
+const pi=3.14;
+//pi= 2.22;------> This will show an error
+console.log(pi);
+
