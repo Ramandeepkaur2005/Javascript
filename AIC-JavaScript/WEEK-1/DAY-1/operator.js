62515f1bd16edfcb1e947036be2d4c657abfb08a
Ramandeepkaur2005 <ramandeepkaur27092005@gmail.com>
2024-02-08 14:26:02 +0530

    day 1

---
+++ AIC-JavaScript/WEEK-1/DAY-1/operator.js
@@ -0,0 +1,45 @@
+//Operators in JS
+//Arithmetic Operator
+let a = 8;
+let b = 4;
+console.log("The difference of a and b is " +(a + b));
+console.log("The addition of a and b is " +(a - b));
+console.log("The multiplication of a and b is " +(a * b));
+console.log("The division ofa and b is " +(a / b));
+console.log("The modulus of a and b is " +(a % b));
+
+ //Assignment operators
+ let x = 6;
+ // x += 6;
+ // x -= 6;
+ // x *= 6
+ x /= 6;
+ console.log(x);
+
+//comparison operators
+let c = 7;
+let d = 8;
+console.log(a == b);
+console.log(a != b);
+console.log(a < b);
+console.log(a > b);
+console.log(a <= b);
+console.log(a >= b);
+
+//logical operators
+let e = 4;
+let f = 7;
+console.log((e == f) && (e >= f));
+console.log((e == f)) || ((e >= f));
+console.log(!(e == f));
+
+//Bitwise operator
+let g = 5;
+let h = 1;
+console.log(g & h);
+console.log(g | h);
+console.log(~g);
+console.log(g ^ h);
+console.log(g << h);
+console.log(g >> h);
+
